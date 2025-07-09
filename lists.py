288e95db2287a84db42d590e59f1712867f07aeb
Md. Anam Rayad Doha <ardoha45@gmail.com>
2025-07-09 13:55:44 +0600

    new file

--- lists.py
+++ lists.py
@@ -1,6 +1,14 @@
-'''A list is a sequenced collection of different objects such as integers, strings, and even other lists as well. The address of each element within a list is called an index. An index is used to access and refer to items within a list.'''
-
+"A list is a sequenced collection of different objects such as integers, strings, and even other lists as well. The address of each element within a list is called an index. An index is used to access and refer to items within a list."
 # Create a list
-
 L = ["The Bodyguard", 7.0, 1992]
 print (L)
+# Output: ['The Bodyguard', 7.0, 1992]
+
+"We can use negative and regular indexing with a list:"
+# Print the elements on each index
+print( L[-1])
+
+
+
+
+
