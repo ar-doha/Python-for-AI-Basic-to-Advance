6159a6d06072c683729f8842b0db70d6aba9b847
Md. Anam Rayad Doha <ardoha45@gmail.com>
2025-07-09 12:57:24 +0600

    Update lists.py

---
+++ lists.py
@@ -0,0 +1,6 @@
+'''A list is a sequenced collection of different objects such as integers, strings, and even other lists as well. The address of each element within a list is called an index. An index is used to access and refer to items within a list.'''
+
+# Create a list
+
+L = ["The Bodyguard", 7.0, 1992]
+print (L)
